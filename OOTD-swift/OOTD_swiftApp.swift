3f5d11f7417c0c3f1c8db7df2c38a965508a2f50
rahqis <rahqisarsour@outlook.com>
2025-05-26 23:44:03 -0400

    registration page

---
+++ OOTD-swift/OOTD_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOTD_swiftApp.swift
+//  OOTD-swift
+//
+//  Created by Rahqi Sarsour on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OOTD_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
